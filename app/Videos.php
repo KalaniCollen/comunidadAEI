e9ac96ee17d74b988100f36c3469d23232a382c0
AEI <proyectoasociacionie@gmail.com>
2018-11-08 16:00:20 -0600

    Correcion de los campos en los modelos

--- app/Videos.php
+++ app/Videos.php
@@ -9,10 +9,8 @@ class Videos extends Model
   protected $table = 'videos';
   protected $primaryKey = 'id_video';
   protected $fillable= [
-<<<<<<< HEAD
-  'Id_Video','Enlace','Codigo','id_usuario',
-=======
+
   'id_video','enlace','codigo','id_usuario','tipo',
->>>>>>> 27670647432db9b2bbc3629bab154ec4abeb3158
+
   ];
 }
