b8398c9bcd7e9a0b9f76d6f6df915922cd849f7d
Aaron Gomez Mendez <aaron.gomez.mendez@gmail.com>
2018-11-11 02:00:51 -0600

    Se agrego laravel collective para agilizar el trabajo con formularios

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         'Sofa\Eloquence\ServiceProvider',
         Laraveles\Spanish\SpanishServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -228,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
