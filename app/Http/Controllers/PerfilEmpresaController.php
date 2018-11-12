995a3bb08546ffc0d24e9cfa3c91203e6c963c42
Aaron Gomez Mendez <aaron.gomez.mendez@gmail.com>
2018-11-12 13:31:23 -0600

    Se agregó el combobox al perfilempresa!

--- app/Http/Controllers/PerfilEmpresaController.php
+++ app/Http/Controllers/PerfilEmpresaController.php
@@ -16,82 +16,76 @@ use Illuminate\Support\Facades\Storage;
 class PerfilEmpresaController extends Controller
 {
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-   public function Index($slug_empresa)
-   {
-      $PerfilEmpresa = Perfil_Empresa::where('slug_empresa',$slug_empresa)->first();
-      $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
-       return view('Perfiles.Empresa.PerfilEmpresa')->with([
-         'perfilE'=>$PerfilEmpresa,
-         'perfil'=>$Perfil]);
-   }
-  public function show($slug_empresa)
-  {
-     $PerfilEmpresa = Perfil_Empresa::where('slug_empresa',$slug_empresa)->first();
-     $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
-      return view('Perfiles.Empresa.EditarPerfilEmpresa')->with([
-        'perfilE'=>$PerfilEmpresa,
-        'perfil'=>$Perfil]);
-  }
-  public function update(Perfilrequest $request)
-  {
-     $PerfilEmpresa = Perfil_Empresa::where('id_usuario',Auth::id())->first();
-// $PerfilEmpresa=$request;
-$PerfilEmpresa->nombre_empresa=Str::upper($request->nombre_empresa);
-$user =User::where('id_usuario',Auth::id())->first();
-$user->slug_empresa=Str::slug($request->nombre_empresa.' '.Auth::id());
-$PerfilEmpresa->slug_empresa=Str::slug($request->nombre_empresa.' '.Auth::id());
-$user->save();
-$PerfilEmpresa->tipo_empresa=Str::upper($request->tipo_empresa);
-$PerfilEmpresa->tipo_empresa=Str::upper($request->tipo_empresa);
-$PerfilEmpresa->giro_empresa=Str::upper($request->giro_empresa);
-$PerfilEmpresa->servicio_empresa=Str::upper($request->servicio_empresa);
-$PerfilEmpresa->producto_empresa=Str::upper($request->producto_empresa);
-$PerfilEmpresa->telefono_fijo_empresa=$request->telefono_fijo_empresa;
-$PerfilEmpresa->horario_atencion=Str::upper($request->horario_atencion);
-$PerfilEmpresa->correo_electronico_empresa=$request->correo_electronico_empresa;
-$PerfilEmpresa->direccion_empresa=$request->direccion_empresa;
-$PerfilEmpresa->red_social_twitter_empresa=$request->red_social_twitter_empresa;
-$PerfilEmpresa->red_social_facebook_empresa=$request->red_social_facebook_empresa;
-$PerfilEmpresa->pag_web_empresa=$request->pag_web_empresa;
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function Index($slug_empresa)
+    {
+        $PerfilEmpresa = Perfil_Empresa::where('slug_empresa',$slug_empresa)->first();
+        $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
+        return view('perfiles.Empresa.PerfilEmpresa')->with([
+            'perfilE'=>$PerfilEmpresa,
+            'perfil'=>$Perfil]);
+    }
+    public function show($slug_empresa)
+    {
+        $PerfilEmpresa = Perfil_Empresa::where('slug_empresa',$slug_empresa)->first();
+        $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
+        return view('perfiles.Empresa.EditarPerfilEmpresa')->with([
+            'perfilE'=>$PerfilEmpresa,
+            'perfil'=>$Perfil]);
+    }
 
-$PerfilEmpresa->slug_empresa = Str::slug(Str::upper($request->nombre_empresa.' '.Auth::id()));
-$perfilU =User::where('id_usuario',Auth::id())->first();
-if($perfilU->tipo_usuario=="Asociado"){
-$PerfilEmpresa->descripcion=Str::upper($request->descripcion);
-}
-$perfilU->slug_empresa=Str::slug(Str::upper($request->nombre_empresa.' '.Auth::id()));
-$perfilU->save();
-
-
-
-
-
-$PerfilEmpresa->save();
-     $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
-      return redirect('PerfilEmpresa/'.$PerfilEmpresa->slug_empresa);
-  }
+    public function update(Perfilrequest $request)
+    {
+        // $PerfilEmpresa = Perfil_Empresa::where('id_usuario',Auth::id())->first();
+        // $PerfilEmpresa->nombre_empresa=Str::upper($request->nombre_empresa);
+        // $user =User::where('id_usuario',Auth::id())->first();
+        // $user->slug_empresa=Str::slug($request->nombre_empresa.' '.Auth::id());
+        // $PerfilEmpresa->slug_empresa=Str::slug($request->nombre_empresa.' '.Auth::id());
+        // $PerfilEmpresa->tipo_empresa=Str::upper($request->tipo_empresa);
+        // $PerfilEmpresa->giro_empresa=Str::upper($request->giro_empresa);
+        // $PerfilEmpresa->telefono_fijo_empresa=$request->telefono_fijo_empresa;
+        // $PerfilEmpresa->horario_atencion=Str::upper($request->horario_atencion);
+        // $PerfilEmpresa->correo_electronico_empresa=$request->correo_electronico_empresa;
+        // $PerfilEmpresa->direccion_empresa=$request->direccion_empresa;
+        // $PerfilEmpresa->red_social_twitter_empresa=$request->red_social_twitter_empresa;
+        // $PerfilEmpresa->red_social_facebook_empresa=$request->red_social_facebook_empresa;
+        // $PerfilEmpresa->pag_web_empresa=$request->pag_web_empresa;
+        //
+        // $PerfilEmpresa->slug_empresa = Str::slug(Str::upper($request->nombre_empresa.' '.Auth::id()));
+        // $perfilU =User::where('id_usuario',Auth::id())->first();
+        // if($perfilU->tipo_usuario=="Asociado"){
+        //     $PerfilEmpresa->descripcion=Str::upper($request->descripcion);
+        // }
+        // $perfilU->slug_empresa=Str::slug(Str::upper($request->nombre_empresa.' '.Auth::id()));
+        //
+        // $perfilU->save();
+        // $PerfilEmpresa->save();
+        // $user->save();
+        // $Perfil=Perfil_Usuario::where('id_usuario',Auth::id())->first();
+        // return redirect('PerfilEmpresa/'.$PerfilEmpresa->slug_empresa);
+        return strcmp($request->oferta, "servicio");
+    }
 
 
-  public function store(Request $request)
-{
-      return view('form.campo')->with('dato', $request);
-  }
+    public function store(Request $request)
+    {
+        return view('form.campo')->with('dato', $request);
+    }
 
 
 
-  public function edit(Request $request)
-  {
-    if ($request->ajax())
-      {
+    public function edit(Request $request)
+    {
+        if ($request->ajax())
+        {
 
-$perfilE =Perfil_Empresa::where('id_usuario',Auth::id())->first();
-          // $perfilE = Perfil_Empresa::FindOrFail($id);
+            $perfilE =Perfil_Empresa::where('id_usuario',Auth::id())->first();
+            // $perfilE = Perfil_Empresa::FindOrFail($id);
 
             Storage::delete(str_replace("/storage", "public", $perfilE->logo_empresa));
 
@@ -102,16 +96,16 @@ $perfilE =Perfil_Empresa::where('id_usuario',Auth::id())->first();
 
 
 
-          if ($result){
-return response()->json(['success'=>'true']);
-              // return view('form.dato')->with('dato', $request);
-          }
-          //else
-          //{
+            if ($result){
+                return response()->json(['success'=>'true']);
+                // return view('form.dato')->with('dato', $request);
+            }
+            //else
+            //{
             //return response()->json(['success'=>'false']);
-          //}
-      }
+            //}
+        }
 
-  }
+    }
 
 }
