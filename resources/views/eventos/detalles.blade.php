5740c3640b73a2952cd3a32a745160432193c06a
AEI <proyectoasociacionie@gmail.com>
2018-11-18 17:19:40 -0600

    Se agrego el registro de usuario

--- resources/views/eventos/detalles.blade.php
+++ resources/views/eventos/detalles.blade.php
@@ -10,6 +10,11 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{$evento->nombre_evento  }}</div>
+                    @if(empty($asistencia))
+                    <a href="registro/{{$evento->id_evento  }}" class="btn btn-primary">Registarse</a>
+                @else Ya estas registrado para este evento
+                @endif
+
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
@@ -25,10 +30,10 @@
                     <div class="panel-body" align="center">
                     <div class="row">
                         <div class="col-md-6">
-                            Fecha inicio: {{$evento->fecha_inicio  }}
+                            Fecha de Inicio: {{$evento->fecha_inicio  }}
                         </div>
                         <div class="col-md-6">
-                            Fecha final: {{$evento->fecha_final  }}
+                            Fecha de termino: {{$evento->fecha_final  }}
                         </div>
                     </div>
                     <br />
@@ -46,13 +51,28 @@
                     <br />
                     <div class="row">
                         <div class="col-md-6">
-                            Precio : ${{$evento->costo_no_asociado  }}
+                            Descripcion: {{$evento->descripcion_evento  }}
                         </div>
                         <div class="col-md-6">
-                            Frecio Asociado: ${{$evento->costo_asociado  }}
+                            Direccion: {{$evento->direccion_evento  }}
                         </div>
                     </div>
-
+                    <div class="row">
+                        <div class="col-md-6">
+                            Ponente: {{$evento->ponente  }}
+                        </div>
+                        <div class="col-md-6">
+                            Organizador: {{$evento->organizador  }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            Correo electronico: {{$evento->correo_electronico_organizador  }}
+                        </div>
+                        <div class="col-md-6">
+                            Telefono: {{$evento->telefono_organizador  }}
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
