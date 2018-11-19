e04c2643d854b2ed6167f3bf685ffbc35886f650
Aaron Gomez Mendez <aaron.gomez.mendez@gmail.com>
2018-11-19 00:01:48 -0600

    Modificacion de rutas y eventos

--- app/Evento.php
+++ app/Evento.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model
 {
-    //
+
+    protected $table = 'evento';
+    protected $primaryKey = 'id_evento';
+    protected $fillable= [
+        'nombre_evento',
+        'descripcion_evento',
+        'fecha_inicio',
+        'fecha_final',
+        'tipo',
+        'num_invitados',
+        'costo_asociado',
+        'costo_no_asociado',
+        'ponente',
+        'organizador',
+        'correo_electronico_organizador',
+        'telefono_organizador',
+        // 'color'
+        'slug_evento',
+        'id_usuario',
+        'direccion_evento',
+    ];
 }
--- app/Registro_Evento.php
+++ app/Registro_Evento.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registro_Evento extends Model
 {
-    //
+    protected $table = 'registro_evento';
+    protected $primaryKey = 'id_registro_evento';
+    protected $fillable= [
+
+    'id_usuario','id_evento',
+
+    ];
 }
