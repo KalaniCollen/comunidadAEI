8227b8f6d6a3ce3a63a5dffa5988e781779b691a
AEI <proyectoasociacionie@gmail.com>
2018-11-20 00:27:46 -0600

    Se corrigio el titulo del panel de control

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Gestor Academico | Panel Control</title>
+    <title>Comunidad AEI | Panel Control</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
  <link rel="shortcut icon" href="img/aei.png" />
