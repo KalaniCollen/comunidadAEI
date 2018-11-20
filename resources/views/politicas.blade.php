df3d0d35199fe8f73a20c40dbfac2f554516042c
AEI <proyectoasociacionie@gmail.com>
2018-11-19 21:04:04 -0600

    Se agrego la vista de politicas y condiciones de uso

---
+++ resources/views/politicas.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.head')
+@section('styles')
+
+@endsection
+@section('content')
+<h2 style="text-align: center;"><strong>POLÍTICA DE PRIVACIDAD</strong></h2>
+<p>&nbsp;</p>
+<br />
+<p>El presente Política de Privacidad establece los términos en que Asociación de Empresarios de Iztapalapa,A.C usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida
+    con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin
+    embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.
+</p>
+<br />
+<p><strong>Información que es recogida</strong></p>
+<p>Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,&nbsp; información de contacto como&nbsp; su dirección de correo electrónica e información demográfica. Así mismo cuando sea necesario podrá ser requerida información
+    específica para procesar algún pedido o realizar una entrega o facturación.
+</p>
+<br />
+<p><strong>Uso de la información recogida</strong></p>
+<p>Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. &nbsp;Es posible que
+    sean enviados correos electrónicos periódicamente a través de nuestro sitio con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante para usted o que pueda brindarle algún beneficio, estos correos
+    electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.</p>
+<p>Asociación de Empresarios de Iztapalapa,A.C está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que no exista ningún
+    acceso no autorizado.</p>
+    <br />
+<p><strong>Cookies</strong></p>
+<p>Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener información respecto al tráfico web, y también
+    facilita las futuras <a href="https://cupondedescuento.com.co/pepe-ganga/" target="_blank">visitas a una web </a> recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerte individualmente y por tanto brindarte
+    el mejor servicio personalizado de su web.</p>
+<p>Nuestro <a href="https://politicadeprivacidadplantilla.com/" target="_blank">sitio web</a> emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis estadístico
+    y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estás no dan acceso a
+    información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente. Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener
+    un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.</p>
+<br />
+<p><strong>Enlaces a Terceros</strong></p>
+<p>Este sitio web pudiera contener en laces a otros sitios que pudieran ser de su interés. Una vez que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos
+    responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted
+    está de acuerdo con estas.</p>
+    <br />
+<p><strong>Control de su información personal</strong></p>
+<p>En cualquier momento usted puede restringir la recopilación o el uso de la información personal que es proporcionada a nuestro sitio web.&nbsp; Cada vez que se le solicite rellenar un formulario, como el de alta de usuario, puede marcar o
+    desmarcar la opción de recibir información por correo electrónico. &nbsp;En caso de que haya marcado la opción de recibir nuestro boletín o publicidad usted puede cancelarla en cualquier momento.</p>
+<p>Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial.</p>
+<p>Asociación de Empresarios de Iztapalapa,A.C Se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.</p>
+<br />
+<h2 style="text-align: center;"><strong>Terminos de uso</strong></h2>
+<br />
+<p><strong>Acceso y navegación en el sitio web: exclusión de garantías y responsabilidad</strong></p>
+<p>Comunidad AEI no garantiza la continuidad, disponibilidad y utilidad del Sitio Web, ni de los Contenidos o Servicios. Comunidad AEI hará todo lo posible por el buen funcionamiento del Sitio Web, sin embargo, no se responsabiliza ni garantiza que
+    el acceso a este Sitio Web no vaya a ser ininterrumpido o que esté libre de error.</p>
+<p>Tampoco se responsabiliza o garantiza que el contenido o software al que pueda accederse a través de este Sitio Web, esté libre de error o cause un daño al sistema informático (software y hardware) del Usuario. &nbsp;En ningún caso Comunidad AEI
+    será responsable por las pérdidas, daños o perjuicios de cualquier tipo que surjan por el acceso, navegación y el uso del Sitio Web, incluyéndose, pero no limitándose, a los ocasionados a los sistemas informáticos o los provocados por la
+    introducción de virus.</p>
+<p>Comunidad AEI no se responsabiliza o garantiza que el contenido o software al que pueda accederse a través de este Sitio Web, esté libre de los derechos de autor, así como el contenido (imágenes, videos, textos, productos, servicios, etc)
+    proporcionado por el usuario este sujeto a derechos de autor, Comunidad AEI no se hace responsable por el mal uso del Sitio Web, así como las infracciones legales realizadas por los usuarios.</p>
+<br />
+<p><strong>El Usuario</strong></p>
+<p>El mero acceso a este Sitio Web no supone entablar ningún tipo de relación de carácter comercial entre Comunidad AEI y el Usuario.</p>
+<p>El Usuario declara ser mayor de edad y disponer de la capacidad jurídica suficiente para vincularse por las presentes Condiciones. &nbsp;Por lo tanto, este Sitio Web de Comunidad AEI no se dirige a menores de edad. Comunidad AEI declina cualquier
+    responsabilidad por el incumplimiento de este requisito.</p>
+<p>Un uso de la información, Contenidos y/o Servicios y datos ofrecidos por Comunidad AEI sin que sea contrario a lo dispuesto por las presentes Condiciones, la Ley, la moral o el orden público, o que de cualquier otro modo puedan suponer lesión de
+    los derechos de terceros o del mismo funcionamiento del Sitio Web.</p>
+<br />
+@endsection
