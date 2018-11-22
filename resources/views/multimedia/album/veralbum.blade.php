d59af818fde34b0a086e48269c4a5bc126e256af
AEI <proyectoasociacionie@gmail.com>
2018-11-22 00:40:33 -0600

    se reahizo la galeria

--- resources/views/multimedia/album/veralbum.blade.php
+++ resources/views/multimedia/album/veralbum.blade.php
@@ -2,8 +2,9 @@
 @section('styles')
     <link rel="stylesheet" href="{{ asset('css/jquery.alertable.css') }}">
     <link href="{{asset('/css/album.css')}}" rel="stylesheet">
-    <link href="{{asset('/css/lightgallery.css')}}" rel="stylesheet">
-
+    {{-- <link href="{{asset('/css/lightgallery.css')}}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{asset('/css/galeria/photoswipe.css')}}">
+        <link rel="stylesheet" href="{{asset('/css/galeria/default-skin.css')}}">
 @endsection
 @section('content')
 
@@ -25,33 +26,100 @@
                 Subir Imagénes
             </button>
         </div>
-        <div class="album-images">
+        <div class="album-images picture three cf" itemscope="" itemtype="http://schema.org/ImageGallery">
+
+
             <ul class="album-images__list" id="js-lightgallery">
+
             @for ($i = 1; $i <= 8; $i++)
-                    <li class="album-images__item" data-src="https://fakeimg.pl/350x200/?text=Hello">
-                        <img class="album-images__img" src="https://fakeimg.pl/350x200/?text=Hello" alt="">
+                <li class="album-images__item">
+                    <figure itemprop="associatedMedia" data="{{$i  }}" itemscope="" itemtype="http://schema.org/ImageObject">
+                        <a href="https://fakeimg.pl/350x200/?text=Hello" class="album-images__item" itemprop="contentUrl" data-size="1000x667" style="width: -webkit-fill-available;">
+                            <img src="https://fakeimg.pl/350x200/?text=Hello" height="400" width="600" itemprop="thumbnail" alt="">
+                        </a>
+                    </figure>
+          {{-- <img class="album-images__img" src="https://fakeimg.pl/350x200/?text=Hello" alt=""> --}}
                     </li>
                 @endfor
             </ul>
         </div>
+        <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true" style="">
+
+    <div class="pswp__bg"></div>
+    <div class="pswp__scroll-wrap">
+
+        <div class="pswp__container" style="transform: translate3d(0px, 0px, 0px);">
+            <div class="pswp__item" style="display: block; transform: translate3d(-1511px, 0px, 0px);">
+							<div class="pswp__zoom-wrap" style="transform: translate3d(240px, 44px, 0px) scale(0.868066);">
+							<div class="pswp__img pswp__img--placeholder pswp__img--placeholder--blank" style="width: 1000px; height: 667px; display: none;"></div>
+							<img class="pswp__img" src="" style="backface-visibility: hidden;"></div></div>
+            <div class="pswp__item" style="transform: translate3d(0px, 0px, 0px);">
+							<div class="pswp__zoom-wrap" style="transform: translate3d(240px, 44px, 0px) scale(0.868066);">
+								<div class="pswp__img pswp__img--placeholder pswp__img--placeholder--blank" style="width: 1000px; height: 667px; display: none;"></div>
+								<img class="pswp__img" src="" style="display: block; backface-visibility: hidden;">
+							</div>
+						</div>
+            <div class="pswp__item" style="display: block; transform: translate3d(1511px, 0px, 0px);"><div class="pswp__zoom-wrap" style="transform: translate3d(240px, 44px, 0px) scale(0.868066);"><div class="pswp__img pswp__img--placeholder pswp__img--placeholder--blank" style="width: 1000px; height: 667px; display: none;"></div><img class="pswp__img" src="" style="backface-visibility: hidden;"></div></div>
+        </div>
+
+        <div class="pswp__ui pswp__ui--fit pswp__ui--hidden">
+
+            <div class="pswp__top-bar">
+
+                <div class="pswp__counter"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">2/3</font></font></div>
+
+                <button class="pswp__button pswp__button--close" title="Cerrar (Esc)"></button>
+                <button class="pswp__button pswp__button--share" title="Compartir"></button>
+                <button class="pswp__button pswp__button--fs" title="Cambiar a pantalla completa"></button>
+                <button class="pswp__button pswp__button--zoom" title="Acercar / alejar"></button>
+
+                <div class="pswp__preloader">
+                    <div class="pswp__preloader__icn">
+                      <div class="pswp__preloader__cut">
+                        <div class="pswp__preloader__donut"></div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                <div class="pswp__share-tooltip"></div>
+            </div>
+
+            <button class="pswp__button pswp__button--arrow--left" title="Anterior (flecha izquierda)">
+            </button>
+
+            <button class="pswp__button pswp__button--arrow--right" title="Siguiente (flecha derecha)">
+            </button>
+
+            <div class="pswp__caption">
+                <div class="pswp__caption__center"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </section>
 @endsection
 @section('scripts')
     <!-- Fine Uploader Thumbnails template w/ customization
     ==================================================================== -->
-    <script src="{{asset('js/lightgallery-all.min.js')}}"></script>
+    {{-- <script src="{{asset('js/lightgallery-all.min.js')}}"></script> --}}
     <script src="{{ asset('/js/jquery.alertable.js')}}" ></script>
     <script src="{{ asset('js/dropzone.js') }}" charset="utf-8"></script>
-
+<script src="{{ asset('js/jquery.min.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('/js/galeria/photoswipe.min.js')}}"></script>
+    <script src="{{ asset('/js/galeria/photoswipe-ui-default.min.js')}}"></script>
+    <script src="{{ asset('/js/galeria/script-min.js')}}"></script>
 
     <script type="text/javascript">
     $(document).ready(function(){
-        $('#js-lightgallery').lightGallery();
-        let jsDropzone = $('#js-dropzone');
-
-        $('#js-album-upload').on('click', function() {
-            $('#js-album-add').toggleClass('album-add--hidden');
-        });
+    //     $('#js-lightgallery').lightGallery();
+    //     let jsDropzone = $('#js-dropzone');
+    //
+    //     $('#js-album-upload').on('click', function() {
+    //         $('#js-album-add').toggleClass('album-add--hidden');
+    //     });
 
         Dropzone.options.jsDropzone = {
             // paramName: 'file',
